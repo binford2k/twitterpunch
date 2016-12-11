3392166ee192907f79d8844a8290843053a661ab
Ben Ford <ben.ford@puppetlabs.com>
2016-12-10 23:30:29 -0800

    Can stream a user instead of a hashtag
    If Twitterpunch is configured with a hashtag, it will stream that. If
    not, it will stream a user. This means that if you send that user a
    tweet, it will be displayed.
    
    Since you can restrict followers, this means a rudimentary access
    control for your slideshow.

--- lib/twitterpunch/streamer.rb
+++ lib/twitterpunch/streamer.rb
@@ -35,57 +35,63 @@ module Twitterpunch
 
     def stream
       begin
-        @client.filter(:track => @config[:hashtag]) do |tweet|
-          if tweet.is_a?(Twitter::Tweet)
-            @logger.info(tweet.text)
-            @logger.log(tweet.user.screen_name, tweet.text)
-
-            content = tweet.text.gsub(/ http\S*/,'').gsub(/#\S*/,'')
-
-            if tweet.media?
-              uri      = tweet.media.first.media_uri
-
-              http     = Net::HTTP.new(uri.host, uri.port)
-              request  = Net::HTTP::Get.new(uri.request_uri)
-              response = http.request(request)
-
-              image    = File.basename uri.path
-
-              File.open("#{@output}/#{image}", 'wb') do |file|
-                file.write(response.body)
-              end
-
-              unless tweet.user.screen_name == @handle
-                @config[:state][image] = content
-              end
-
-              if @viewer
-                @viewer.pop(image, content)
-              else
-                # OS X screensaver doesn't reload images dynamically. This kinda sucks.
-                if RUBY_PLATFORM =~ /darwin/ and system('pgrep ScreenSaverEngine >/dev/null')
-                  system('osascript', '-e', 'tell application "System Events" to stop current screen saver')
-                  system('osascript', '-e', 'tell application "System Events" to start current screen saver')
-                end
-              end
-
-            end
-
-            unless tweet.user.screen_name == @handle
-              message = "#{tweet.user.name} says #{content}"
-
-              case RUBY_PLATFORM
-              when /mingw|cygwin/
-                system('cscript', "#{@config[:resources]}/say.vbs", message)
-              when /darwin/
-                system('say', message)
-              end
-            end
-
-          end
+        if @config[:hashtag]
+          @client.filter(:track => @config[:hashtag]) { |tweet| handle(tweet) }
+        else
+          @client.user { |tweet| handle(tweet) }
         end
       rescue Interrupt => e
         @logger.error "Exiting: #{e.message}"
+      end
+    end
+
+    def handle(tweet)
+      if tweet.is_a?(Twitter::Tweet)
+        @logger.info(tweet.text)
+        @logger.log(tweet.user.screen_name, tweet.text)
+
+        content = tweet.text.gsub(/http\S*/,'').gsub(/#\S*/,'').gsub(/@#{@config[:handle]}/, '')
+
+        if tweet.media?
+          uri      = tweet.media.first.media_uri
+
+          http     = Net::HTTP.new(uri.host, uri.port)
+          request  = Net::HTTP::Get.new(uri.request_uri)
+          response = http.request(request)
+
+          image    = File.basename uri.path
+
+          File.open("#{@output}/#{image}", 'wb') do |file|
+            file.write(response.body)
+          end
+
+          unless tweet.user.screen_name == @handle
+            @config[:state][image] = content
+          end
+
+          if @viewer
+            @viewer.pop(image, content)
+          else
+            # OS X screensaver doesn't reload images dynamically. This kinda sucks.
+            if RUBY_PLATFORM =~ /darwin/ and system('pgrep ScreenSaverEngine >/dev/null')
+              system('osascript', '-e', 'tell application "System Events" to stop current screen saver')
+              system('osascript', '-e', 'tell application "System Events" to start current screen saver')
+            end
+          end
+
+        end
+
+        unless tweet.user.screen_name == @handle
+          message = "#{tweet.user.name} says #{content}"
+
+          case RUBY_PLATFORM
+          when /mingw|cygwin/
+            system('cscript', "#{@config[:resources]}/say.vbs", message)
+          when /darwin/
+            system('say', message)
+          end
+        end
+
       end
     end
 
