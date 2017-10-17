fdbac8fbe41d49a586a3103058e6585342d699d3
Ben Ford <ben.ford@puppetlabs.com>
2017-10-16 23:10:56 -0700

    Allow you to configure which photo booth app to use
    When running the remote, it just needs to know the app title and the
    keystroke it should send that app.

--- lib/twitterpunch/remote.rb
+++ lib/twitterpunch/remote.rb
@@ -9,12 +9,15 @@ class Twitterpunch::Remote < Sinatra::Base
   set :views, File.dirname(__FILE__) + '/../../views'
   set :public_folder, File.dirname(__FILE__) + '/../../public'
   set :erb, :trim => '-'
-  set :configfile,  File.expand_path('~/.twitterpunch/queue.yaml')
 
   configure :production, :development do
     enable :logging
     enable :sessions
-    system('osascript', '-e' 'tell application "Photo Booth" to activate')
+  end
+
+  def initialize(app=nil)
+    super(app)
+    system('osascript', '-e' "tell application \"#{settings.remote[:apptitle]}\" to activate")
   end
 
   get '/' do
@@ -33,15 +36,19 @@ class Twitterpunch::Remote < Sinatra::Base
   helpers do
     def photo
       begin
-        stdout, status = Open3.capture2e('osascript', '-e', 'tell application "Photo Booth" to activate')
-        puts stdout
-        raise "Could not activate Photo Booth" unless status.success?
+        stdout, status = Open3.capture2e('osascript', '-e', "tell application \"#{settings.remote[:apptitle]}\" to activate")
+        unless status.success?
+          puts stdout
+          raise "Could not activate Photo Booth"
+        end
 
         # This is kind of iffy, because it depends on having full control over the UI.
-        # This will only work when Photo Booth actually has the foreground.
-        stdout, status = Open3.capture2e('osascript', '-e', 'tell application "System Events" to keystroke return')
-        puts stdout
-        raise "Snapshot failed" unless status.success?
+        # This will only work when the Photo Booth app actually has the foreground.
+        stdout, status = Open3.capture2e('osascript', '-e', "tell application \"System Events\" to keystroke #{settings.remote[:hotkey]}")
+        unless status.success?
+          puts stdout
+          raise "Snapshot failed"
+        end
 
         'ok'
       rescue => e
